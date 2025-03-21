c2fad786dcdababbee0329f28dbe164e2f22b959
sorooshkj <soroush.kj@gmail.com>
2025-03-21 20:56:26 +0100

    Solved center align problem

--- src/pages/all movie/movies.jsx
+++ src/pages/all movie/movies.jsx
@@ -10,26 +10,24 @@ function Movies() {
     const { movies, loading, handleDelete } = useContext(MoviesContext);
 
     return (
-        <div>
-            <Wrapper>
-                <div>
-                    <Navbar />
-                    <div className="px-4 py-8 max-w-screen-xl mx-auto">
-                        {loading ? (
-                            <div className="text-white text-center text-lg">Loading...</div>
-                        ) : (
-                            <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] place-items-center gap-6 px-4 max-w-screen-xl mx-auto p-5">
-                                {movies.map((movie) => (
-                                    <Suspense fallback={<div>Loading movie...</div>} key={movie._id}>
-                                        <Singlemovie article={movie} handleDelete={handleDelete} />
-                                    </Suspense>
-                                ))}
-                            </div>
-                        )}
-                    </div>
+        <Wrapper>
+            <div>
+                <Navbar />
+                <div className="px-4 py-8 m-auto max-w-screen-xl mx-auto">
+                    {loading ? (
+                        <div className="text-white text-center text-lg">Loading...</div>
+                    ) : (
+                        <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] place-items-center gap-6 px-4 max-w-screen-xl mx-auto p-5">
+                            {movies.map((movie) => (
+                                <Suspense fallback={<div>Loading movie...</div>} key={movie._id}>
+                                    <Singlemovie article={movie} handleDelete={handleDelete} />
+                                </Suspense>
+                            ))}
+                        </div>
+                    )}
                 </div>
-            </Wrapper>
-        </div>
+            </div>
+        </Wrapper>
 
     );
 }
